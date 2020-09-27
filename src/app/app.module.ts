0007f7259e0d58730eed9d657162fbc53bd1a9fe
Mohamed Amrani <38511105+MedAmrani@users.noreply.github.com>
2020-09-27 21:29:41 +0100

    header completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/firebase.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBBrEyqMkVwH_Hr4v1aVsruQEkXDEuxZ9U",
+    authDomain: "facebook-clone-b69d2.firebaseapp.com",
+    databaseURL: "https://facebook-clone-b69d2.firebaseio.com",
+    projectId: "facebook-clone-b69d2",
+    storageBucket: "facebook-clone-b69d2.appspot.com",
+    messagingSenderId: "475545870265",
+    appId: "1:475545870265:web:2b0a139a832334405e6068",
+    measurementId: "G-C2EHPRSNF2"
+  };
